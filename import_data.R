c03708968b487f0e2913bd1507dfe7a5a0746796
cameronosmith <cos008@ucsd.edu>
2019-04-09 19:53:30 -0700

    added import data script for ppl

---
+++ import_data.R
@@ -0,0 +1,33 @@
+#for references
+dataset_path = "datasets"
+os_sep = "/"
+#read in all the datas and only get value col
+ENSO_data 	<- read.csv(paste(dataset_path, "ENSO_Clean.csv", sep=os_sep))
+NAO_data 	<- read.csv(paste(dataset_path, "NAO_Clean.csv", sep=os_sep))
+NPI_data 	<- read.csv(paste(dataset_path, "NPI_Clean.csv", sep=os_sep))
+PDO_data 	<- read.csv(paste(dataset_path, "PDO_Clean.csv", sep=os_sep))
+
+#for  asier reference
+indices_list = list(ENSO_data, NAO_data, NPI_data, PDO_data)
+
+#get number of shared date index rows 
+first_year_date <- "1900-03-01"
+num_shared_rows <- .Machine$integer.max
+for (dataset in indices_list) {
+	min_row 	<- which(first_year_date == dataset$Date)[[1]]
+	total_row 	<- nrow(dataset) - min_row
+	if (total_row < num_shared_rows) num_shared_rows = total_row
+}
+
+#construct our dataset format with only data that overlaps in date
+modes_df = data.frame(matrix(nrow=num_shared_rows, ncol=length(indices_list)))
+for (idx in seq_along(indices_list)) {
+	dataset		<- indices_list[[idx]]
+	idx_name 	<- toString(dataset$Index[[1]])
+	#set col data
+	colnames(modes_df)[[idx]] <- idx_name
+	min_row 	<- which(first_year_date == dataset$Date)[[1]]
+	top_row 	<- min_row + num_shared_rows-1
+	shared_rows <- dataset[ min_row:top_row, "Value" ]
+	modes_df[idx_name] <- shared_rows
+}
